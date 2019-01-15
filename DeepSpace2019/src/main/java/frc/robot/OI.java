81cb010f35de97ba6dc3e32ce9f68b6954bbdf96
Byte11 <shirmak11@gmail.com>
2019-01-15 16:51:16 -0600

    Imported Old Code and Deleted Example Classes
    I've imported all of the IO and the Robot map code that we need. Everything else will be new for this year.

--- DeepSpace2019/src/main/java/frc/robot/OI.java
+++ DeepSpace2019/src/main/java/frc/robot/OI.java
@@ -7,36 +7,88 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+  
+	public static Joystick xboxController;
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+	// Xbox Button mappings
+	public final static int AButtonNum = 1;
+	public final static int BButtonNum = 2;
+	public final static int YButtonNum = 4;
+	public final static int XButtonNum = 3;
+	public final static int left_Bumper = 5;
+	public final static int right_Bumper = 6;
+	public final static int select_Button = 6;
+	public final static int start_Button = 7;
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
+	// Xbox axis numbers
+	public final static int leftYAxis = 1;
+	public final static int leftXAxis = 0;
+	public final static int rightYAxis = 5;
+	public final static int rightXAxis = 4;
 
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
+	public final static int left_Trigger = 2;
+	public final static int right_Trigger = 3;
 
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
+	public JoystickButton AButton, BButton, YButton, XButton;
 
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+	public OI() {
+		xboxController = new Joystick(RobotMap.xboxPort);
+
+		AButton = new JoystickButton(xboxController, AButtonNum);
+		BButton = new JoystickButton(xboxController, BButtonNum);
+		YButton = new JoystickButton(xboxController, YButtonNum);
+		XButton = new JoystickButton(xboxController, XButtonNum);
+
+	}
+
+	public static double getXboxLeftXAxis() {
+		double x_axis = xboxController.getRawAxis(leftXAxis);
+
+		if (x_axis > -.2 && x_axis < .2)
+			x_axis = 0;
+
+		return x_axis;
+	}
+
+	public static double getXboxLeftYAxis() {
+		double y_axis = xboxController.getRawAxis(leftYAxis);
+
+		if (y_axis > -.2 && y_axis < .2)
+			y_axis = 0;
+
+		return -y_axis;
+	}
+
+	public static double getXboxRightXAxis() {
+		double x_axis = xboxController.getRawAxis(rightXAxis);
+
+		if (x_axis > -.2 && x_axis < .2)
+			x_axis = 0;
+
+		return -x_axis;
+	}
+
+	public static double getTriggerValue() {
+		double leftTriggerSpeed = xboxController.getRawAxis(left_Trigger);
+		if (leftTriggerSpeed < .1) {
+			leftTriggerSpeed = 0;
+		}
+
+		double rightTriggerSpeed = xboxController.getRawAxis(right_Trigger);
+		if (rightTriggerSpeed < .1) {
+			rightTriggerSpeed = 0;
+		}
+
+		double triggerSpeed = rightTriggerSpeed - leftTriggerSpeed;
+
+		return triggerSpeed;
+	}
 }

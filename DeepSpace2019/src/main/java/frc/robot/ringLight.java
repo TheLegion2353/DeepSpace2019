78d0eace24700e06362c6b9d8d2fcc652fe09831
Geeoon <geeoon13@gmail.com>
2019-02-04 15:54:56 -0600

    Added ringLight java class.
    Class has setOn, setOff, and toggle functions.

---
+++ DeepSpace2019/src/main/java/frc/robot/ringLight.java
@@ -0,0 +1,27 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
+
+public class ringLight {
+    DigitalOutput hBridge;
+
+    public void init() {
+        hBridge = new DigitalOutput(2);
+    }
+
+    public void setOn() {
+        hBridge.set(true);
+    }
+
+    public void setOff() {
+        hBridge.set(false);
+    }
+
+    public void toggle() {
+        if (hBridge.get() == true) {
+            hBridge.set(false);
+        } else {
+            hBridge.set(true);
+        }
+    }
+}
